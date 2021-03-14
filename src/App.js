f2a2265358f0b72dd5bb49934c9f4df1f6633666
Mitleshwar <58841117+Mitlesh29@users.noreply.github.com>
2021-03-14 12:26:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BarChart from './Components/BarChart'
+import './App.css';
+
+const App= () => {
+  return(
+    <div>
+      <BarChart/>
+    </div>
+  )
+}
+
+export default App
